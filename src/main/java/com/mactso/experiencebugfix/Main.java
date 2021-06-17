605e7817ddefaf7f1ff88b337001d4d6d9886e3d
MacTso <9662494+mactso@users.noreply.github.com>
2021-06-16 19:05:10 -0500

    Version 1	Minecraft 1.16.1-1.16.5 Forge 36.1.0

---
+++ src/main/java/com/mactso/experiencebugfix/Main.java
@@ -0,0 +1,15 @@
+package com.mactso.experiencebugfix;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("experiencebugfix")
+public class Main {
+
+	    public static final String MODID = "experiencebugfix"; 
+	    
+	    public Main()
+	    {
+	    	System.out.println(MODID + ": Registering Mod.");
+	    }
+
+}
---
+++ src/main/java/com/mactso/experiencebugfix/events/ChangeDimension.java
@@ -0,0 +1,27 @@
+package com.mactso.experiencebugfix.events;
+
+
+import com.mactso.experiencebugfix.Main;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraftforge.event.entity.player.PlayerEvent.PlayerChangedDimensionEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+
+
+@Mod.EventBusSubscriber(modid = Main.MODID, bus = Bus.FORGE)
+public class ChangeDimension
+{
+	@SubscribeEvent
+	public static void onChange(PlayerChangedDimensionEvent event)
+	{
+		PlayerEntity ep = event.getPlayer();
+		if (ep instanceof ServerPlayerEntity)
+		{
+			ServerPlayerEntity player = (ServerPlayerEntity) ep;
+			player.setExperienceLevel(player.experienceLevel);
+		}
+	}
+}
